647b6df3346b373b2d282332da842eb6c0e904a7
Omkar Upadhyay <omkarmh009@gmail.com>
2024-03-18 07:14:15 +0800

    E-Commerce Maven Project generated using spring.io.initializer

---
+++ batch-service/src/main/java/com/ou_solutions/batchservice/BatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ou_solutions.batchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchServiceApplication.class, args);
+	}
+
+}
